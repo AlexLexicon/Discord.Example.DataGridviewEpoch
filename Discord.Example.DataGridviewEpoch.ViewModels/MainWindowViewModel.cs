112336ca048bcbc82c121410ba0d6a4a1479d621
Alex Stroot <alexrs.business@outlook.com>
2022-12-15 16:51:50 -0800

    Add project files.

---
+++ Discord.Example.DataGridviewEpoch.ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+
+namespace Discord.Example.DataGridviewEpoch.ViewModels;
+public class MainWindowViewModel
+{
+
+    public MainWindowViewModel()
+    {
+        MyTable = new DataTable();
+
+        MyTable.Columns.Add("Name");
+        MyTable.Columns.Add("DateTime");
+
+        for (int row = 0; row < 5; row++)
+        {
+            DataRow myRow = MyTable.NewRow();
+            myRow["Name"] = $"Row: {row + 1}";
+            myRow["DateTime"] = DateTimeOffset.Now.ToUnixTimeSeconds();
+            MyTable.Rows.Add(myRow);
+        }
+    }
+
+    public DataTable MyTable { get; }
+}
---
+++ Discord.Example.DataGridviewEpoch.WPF/DataTimeColumnConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Discord.Example.DataGridviewEpoch.WPF;
+public class DataTimeColumnConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string stringValue && long.TryParse(stringValue, out long epochSeconds))
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(epochSeconds).DateTime.ToString();
+        }
+
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
